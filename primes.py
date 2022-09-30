28e5519ca49524607a105ef53914a0cdebf34b9c
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-09-30 13:20:27 +0100

    Generate an n-sized list of prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Cannot be called with zero or negative number.")
+
     list = []
+
+    primesRemaining = number_of_primes
+    currentNumber = 2
+
+    while primesRemaining != 0:
+        isCurrentNumberPrime = True
+
+        for n in range(2, currentNumber - 1):
+            if currentNumber % n == 0:
+                isCurrentNumberPrime = False
+                break
+
+        if isCurrentNumberPrime:
+            primesRemaining -= 1
+            list.append(currentNumber)
+
+        currentNumber += 1
+
     return list
